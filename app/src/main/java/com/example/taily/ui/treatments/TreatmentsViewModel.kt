9ec11e8016f484075a266b322a206ed24cfec415
Daniela Castellanos <ld.castellanos@uniandes.edu.co>
2023-03-16 02:59:10 -0500

    Treatments

---
+++ app/src/main/java/com/example/taily/ui/treatments/TreatmentsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.taily.ui.treatments
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class TreatmentsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
