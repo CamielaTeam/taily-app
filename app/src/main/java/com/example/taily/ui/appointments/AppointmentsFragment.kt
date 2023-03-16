50bf1634b77f34ec4a6c9b7a14f5875287823a2a
Camilo Alejandro Sánchez Cruz <ca.sanchezc12@uniandes.edu.co>
2023-03-16 02:37:18 -0500

    Pets and appointments (#2)

--- app/src/main/java/com/example/taily/ui/appointments/AppointmentsFragment.kt
+++ app/src/main/java/com/example/taily/ui/appointments/AppointmentsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.taily.ui.notifications
+package com.example.taily.ui.appointments
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.taily.databinding.FragmentNotificationsBinding
+import com.example.taily.databinding.FragmentAppointmentsBinding
 
-class NotificationsFragment : Fragment() {
+class AppointmentsFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentAppointmentsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,16 +22,13 @@ class NotificationsFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-                ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val appointmentsViewModel =
+                ViewModelProvider(this).get(AppointmentsViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentAppointmentsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
         return root
     }
 
